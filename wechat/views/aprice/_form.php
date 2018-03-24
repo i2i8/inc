fe0bd8cd894de9e48e49fb90aeb74bb10de5ca70
root <root@me.(none)>
2018-03-24 18:44:04 +0800

    aprice_form

---
+++ wechat/views/aprice/_form.php
@@ -0,0 +1,52 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
+use wechat\models\Amodel;
+use wechat\models\Atypes;
+use yii\helpers\ArrayHelper;
+use kartik\depdrop\DepDrop;
+
+/* @var $this yii\web\View */
+/* @var $model wechat\models\Aprice */
+/* @var $form yii\widgets\ActiveForm */
+?>
+<div class="aprice-form">
+
+   <?php $form = ActiveForm::begin(['id' => 'dynamic-form']); ?>
+
+   <?= $form->field($model, 'model_amid')->dropDownList(Amodel::getModelname(),
+       [
+           'prompt' => 'Select Model',
+           'id' => 'pid',
+       ]
+    );
+   ?>
+
+    
+    <?= $form->field($model, 'types_atid')->widget(DepDrop::classname(), 
+        [
+            'data' => Atypes::getSubCatList($model->model_amid),
+            'options'=>['id'=>'sid'],
+            'pluginOptions'=>
+            [
+                'depends'=>['pid'],
+                'placeholder'=>'Select Types...',
+                'url'=>Url::to(['/atypes/smm'])
+            ]
+        ]
+    );
+    ?>
+
+    <?= $form->field($model, 'nowprice')->textInput() ?>
+
+    <?= $form->field($model, 'willprice')->textInput() ?>
+    
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-info','style'=>'width:100%;']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
